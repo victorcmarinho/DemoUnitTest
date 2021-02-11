073b276516832079cf86a6fde60a92db8b870c27
Victor Marinho <victorcmarinho@gmail.com>
2021-02-11 13:51:48 -0300

    init

--- __tests__/src/page/home.spec.tsx
+++ __tests__/src/page/home.spec.tsx
@@ -1,56 +1,9 @@
 import React from 'react';
-import { Home } from '../../../src/Pages/Home';
-import { fireEvent, render } from '@testing-library/react-native';
 import { act } from 'react-test-renderer';
 
+import { fireEvent, render } from '@testing-library/react-native';
+
 import * as countHook from '../../../src/context/count';
+import { Home } from '../../../src/Pages/Home';
 
-describe('<Home/>', () => {
-  const decrement = jest.fn();
-  const increment = jest.fn();
-  beforeEach(() => {
-    jest.spyOn(countHook, 'useCount').mockReturnValue({
-      count: 0,
-      decrement,
-      increment,
-    });
-  });
-
-  it('Should be able render', () => {
-    const component = render(<Home />);
-
-    expect(component).toBeTruthy();
-  });
-
-  it('Should be able press add button', () => {
-    const { getByTestId } = render(<Home />);
-    const button = getByTestId('add-button');
-
-    act(() => {
-      fireEvent.press(button);
-    });
-
-    expect(increment).toBeCalled();
-  });
-
-  it('Should be able press remove button', () => {
-    const { getByTestId } = render(<Home />);
-    const button = getByTestId('remove-button');
-
-    act(() => {
-      fireEvent.press(button);
-    });
-
-    expect(decrement).toBeCalled();
-  });
-
-  it('Should be able insert input value', () => {
-    const { getByTestId } = render(<Home />);
-    const input = getByTestId('input');
-
-    act(() => {
-      fireEvent.changeText(input, '77788899911');
-    });
-    expect(input.props.value).toBe('777.888.999-11');
-  });
-});
+describe('<Home/>', () => {});
