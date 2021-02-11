3a7d1b9460008239ff74144dd5f3571a3afbe4d3
Victor Marinho <victorcmarinho@gmail.com>
2021-02-11 14:18:10 -0300

    husahausas

--- __tests__/src/page/home.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { act } from 'react-test-renderer';
-
-import { fireEvent, render } from '@testing-library/react-native';
-
-import * as countHook from '../../../src/context/count';
-import { Home } from '../../../src/Pages/Home';
-
-describe('<Home/>', () => {});
